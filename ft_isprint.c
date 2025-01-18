e7b5d074efab1de3fa7758b0efbab3106491213b
Fiorella G. <fleurelle.g@gmail.com>
2025-01-18 20:41:35 +0100

    Update ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -18,23 +18,3 @@ int	ft_isprint(int c)
 		return (1);
 	return (0);
 }
-/*
-int	main(void)
-{
-	char	test_isprint[] = {'!', '\n', ' ', '@', '\f', '%', '\t'};
-	int	size = sizeof(test_isprint) / sizeof(test_isprint[0]);
-	int	i;
-
-	printf("Pruebas de isprint:\n");
-	i = 0;
-	while (i < size)
-	{
-		char	c = test_isprint[i];
-		if (ft_isprint(c))
-			printf("El caracter '%c' es printable.\n", c);
-		else
-			printf("El caracter '%c' no es printable.\n", c);
-		i++;
-	}
-	return (0);
-}*/
