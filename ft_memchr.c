c1fe617a80d93a4f8f04b68b25f6d94c9cafd294
Fiorella G. <fleurelle.g@gmail.com>
2025-01-18 20:50:07 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,19 +12,19 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *str, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*s;
+	const unsigned char	*str;
 	unsigned char		ch;
 	size_t				i;
 
-	s = (const unsigned char *)str;
+	str = (const unsigned char *)s;
 	ch = (unsigned char )c;
 	i = 0;
 	while (i < n)
 	{
-		if (s[i] == ch)
-			return ((void *)(s + i));
+		if (str[i] == ch)
+			return ((void *)(str + i));
 		i++;
 	}
 	return (NULL);
