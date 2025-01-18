e2bb85df040c8c444c1ff0633ddcf4d939914bba
Fiorella G. <fleurelle.g@gmail.com>
2025-01-18 20:58:52 +0100

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -16,13 +16,3 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-/*
-int	main(void)
-{
-	ft_putchar_fd('C', 1);
-	ft_putchar_fd('O', 1);
-	ft_putchar_fd('D', 1);
-	ft_putchar_fd('E', 1);
-	ft_putchar_fd('\n', 1);
-	return (0);
-}*/
